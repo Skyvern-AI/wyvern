03e6ff15d1d6b228f3984f25b56a11787bb66b3e
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-08-16 18:59:02 -0700

    remove logs in httpx

--- wyvern/core/httpx.py
+++ wyvern/core/httpx.py
@@ -16,24 +16,14 @@ class HTTPXClientWrapper:
     def start(self):
         """Instantiate the client. Call from the FastAPI startup hook."""
         self.async_client = httpx.AsyncClient(timeout=DEFAULT_HTTPX_TIMEOUT)
-        logger.info(f"httpx AsyncClient instantiated. Id {id(self.async_client)}")
 
     async def stop(self):
         """Gracefully shutdown. Call from FastAPI shutdown hook."""
         if not self.async_client:
             return
-        logger.info(
-            f"httpx async_client.is_closed(): {self.async_client.is_closed} - Now close it. "
-            f"Id (will be unchanged): {id(self.async_client)}",
-        )
         if self.async_client and not self.async_client.is_closed:
             await self.async_client.aclose()
-        logger.info(
-            f"httpx async_client.is_closed(): {self.async_client.is_closed}. "
-            f"Id (will be unchanged): {id(self.async_client)}",
-        )
         self.async_client = None
-        logger.info("httpx AsyncClient closed")
 
     def __call__(self):
         """Calling the instantiated HTTPXClientWrapper returns the wrapped singleton."""
