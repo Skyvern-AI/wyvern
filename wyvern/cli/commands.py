17d2b18e4430a7cc0792f1225f6cf21b2baccbd6
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-08-20 23:10:45 -0700

    update wyvern init and wyvern run

--- wyvern/cli/commands.py
+++ wyvern/cli/commands.py
@@ -1,33 +1,70 @@
 # -*- coding: utf-8 -*-
+import getpass
 import importlib
 import os
+import shutil
+import zipfile
 from pathlib import Path
-from typing import Dict, List
+from typing import Optional
 
+import requests
 import typer
 import uvicorn
 from typing_extensions import Annotated
 
 app = typer.Typer()
+WYVERN_TEMPLATE_URL = "https://codeload.github.com/Wyvern-AI/wyvern-starter/zip/main"
 
-# Define the project structure
-PROJECT_STRUCTURE = {
-    "pipelines": ["main.py"],
-    "feature-store-python": {
-        "features": ["features.py", "main.py"],
-    },
-    "ci": ["deploy.py"],
-    ".env": None,
-    ".gitignore": None,
-}
+
+def _replace_info(project: str, author: Optional[str] = None):
+    toml_file_path = os.path.join(project, "pyproject.toml")
+    if os.path.isfile(toml_file_path):
+        author = author or getpass.getuser() or ""
+        with open(toml_file_path, "r") as file:
+            file_content = file.read()
+        file_content = file_content.replace("wyvern-starter", project)
+        file_content = file_content.replace('authors = [""]', f'authors = ["{author}"]')
+        with open(toml_file_path, "w") as file:
+            file.write(file_content)
 
 
 @app.command()
 def init(
-    project_path: str = typer.Argument(..., help="Path to initialize the project"),
+    project: str = typer.Argument(..., help="Name and path to initialize the project"),
 ):
-    typer.echo("Initializing your ML application with Wyvern...")
-    dfs_create_file(project_path, PROJECT_STRUCTURE)
+    typer.echo("Initializing Wyvern application template code...")
+    if Path(project).exists():
+        typer.echo(f"Error: Destination path '{project}' already exists.")
+        return
+
+    response = requests.get(WYVERN_TEMPLATE_URL)
+
+    if response.status_code == 200:
+        with open("temp.zip", "wb") as temp_zip:
+            temp_zip.write(response.content)
+
+        with zipfile.ZipFile("temp.zip", "r") as zip_ref:
+            zip_ref.extractall(project)
+
+        os.remove("temp.zip")
+        # Flatten the extracted content into the destination directory
+        extracted_dir = os.path.join(project, os.listdir(project)[0])
+        for item in os.listdir(extracted_dir):
+            item_path = os.path.join(extracted_dir, item)
+            if os.path.isfile(item_path):
+                shutil.move(item_path, os.path.join(project, item))
+            elif os.path.isdir(item_path):
+                shutil.move(item_path, os.path.join(project, item))
+        shutil.rmtree(extracted_dir)
+
+        # replace the project name in pyproject.toml
+        _replace_info(project)
+
+        typer.echo(
+            f"Successfully initialized Wyvern application template code in {project}",
+        )
+    else:
+        typer.echo(f"Error: Unable to download code from {WYVERN_TEMPLATE_URL}")
 
 
 @app.command()
@@ -36,7 +73,7 @@ def run(
     host: Annotated[
         str,
         typer.Option(help="Host to run the application on"),
-    ] = "127.0.0.1",
+    ] = "0.0.0.0",
     port: Annotated[
         int,
         typer.Option(help="Port to run the application on. Default port is 5001"),
@@ -58,37 +95,3 @@ def run(
     )
     uvicorn_server = uvicorn.Server(config=config)
     uvicorn_server.run()
-
-
-def create_file(file_path, content):
-    if Path(file_path).exists():
-        raise FileExistsError(f"File already exists: {file_path}")
-    with open(file_path, "w") as f:
-        f.write(content)
-
-
-def dfs_create_file(path: str, structure: Dict | List | str | None = None) -> None:
-    if structure is None:
-        # create a file
-        create_file(path, "")
-    elif isinstance(structure, str):
-        # create the directory with path
-        Path(path).mkdir(parents=True, exist_ok=True)
-        # create the child file
-        create_file(os.path.join(path, structure), "")
-    elif isinstance(structure, list):
-        # create the directory with path
-        Path(path).mkdir(parents=True, exist_ok=True)
-        # create the child files
-        for child in structure:
-            if not isinstance(child, str):
-                raise ValueError(f"Invalid value under {path}: {type(child)} - {child}")
-            create_file(os.path.join(path, child), "")
-    elif isinstance(structure, dict):
-        # create the directory with path
-        Path(path).mkdir(parents=True, exist_ok=True)
-        # create the child files
-        for child_path, child_structure in structure.items():
-            dfs_create_file(os.path.join(path, child_path), child_structure)
-    else:
-        raise ValueError(f"Unknown type: {type(structure)} - {structure}")
