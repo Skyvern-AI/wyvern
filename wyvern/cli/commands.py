041405b55c4bb335d76ea7025513671f57377b61
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-08-22 19:14:30 -0700

    add  command to help install redis

--- wyvern/cli/commands.py
+++ wyvern/cli/commands.py
@@ -2,7 +2,9 @@
 import getpass
 import importlib
 import os
+import platform
 import shutil
+import subprocess
 import zipfile
 from pathlib import Path
 from typing import Optional
@@ -10,10 +12,15 @@ from typing import Optional
 import requests
 import typer
 import uvicorn
+from platformdirs import user_data_dir
 from typing_extensions import Annotated
 
+REDIS_VERSION = "redis-6.2.9"
 app = typer.Typer()
 WYVERN_TEMPLATE_URL = "https://codeload.github.com/Wyvern-AI/wyvern-starter/zip/main"
+REDIS_URL = f"https://download.redis.io/releases/{REDIS_VERSION}.tar.gz"
+REDIS_DIR = os.path.join(user_data_dir("redis_cli"), "redis")
+REDIS_BIN = os.path.join(REDIS_DIR, REDIS_VERSION, "src", "redis-server")
 
 
 def _replace_info(project: str, author: Optional[str] = None):
@@ -26,6 +33,75 @@ def _replace_info(project: str, author: Optional[str] = None):
         file_content = file_content.replace('authors = [""]', f'authors = ["{author}"]')
         with open(toml_file_path, "w") as file:
             file.write(file_content)
+
+
+def is_redis_installed():
+    return os.path.exists(REDIS_BIN)
+
+
+def is_redis_running():
+    if platform.system().lower() == "windows":
+        try:
+            subprocess.run(
+                ["tasklist"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=True,
+            )
+        except subprocess.CalledProcessError:
+            return False
+        return (
+            "redis-server.exe"
+            in subprocess.run(["tasklist"], stdout=subprocess.PIPE).stdout.decode()
+        )
+
+    else:  # For Unix-like systems (Linux, macOS)
+        try:
+            subprocess.run(
+                ["ps", "-A"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=True,
+            )
+        except subprocess.CalledProcessError:
+            return False
+        return (
+            "redis-server"
+            in subprocess.run(["ps", "-A"], stdout=subprocess.PIPE).stdout.decode()
+        )
+
+
+def try_install_redis():
+    if is_redis_installed():
+        typer.echo("Redis is already installed.")
+        return
+
+    typer.echo(f"Installing Redis in {REDIS_DIR}...")
+    os.makedirs(REDIS_DIR, exist_ok=True)
+    subprocess.run(
+        ["curl", "-L", REDIS_URL, "-o", os.path.join(REDIS_DIR, "redis.tar.gz")],
+    )
+    subprocess.run(
+        ["tar", "xzvf", os.path.join(REDIS_DIR, "redis.tar.gz")],
+        cwd=REDIS_DIR,
+    )
+    subprocess.run(["make"], cwd=os.path.join(REDIS_DIR, REDIS_VERSION))
+    shutil.move(
+        os.path.join(REDIS_DIR, REDIS_VERSION, "src", "redis-server"),
+        REDIS_BIN,
+    )
+
+
+def start_redis():
+    try_install_redis()
+
+    if is_redis_running():
+        typer.echo("Redis is already running.")
+        return
+
+    typer.echo("Starting Redis...")
+
+    subprocess.run([REDIS_BIN])
 
 
 @app.command()
@@ -99,3 +175,9 @@ def run(
     )
     uvicorn_server = uvicorn.Server(config=config)
     uvicorn_server.run()
+
+
+@app.command()
+def redis() -> None:
+    """Starts Redis server."""
+    start_redis()
