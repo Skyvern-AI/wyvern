2c44e99d4ed9809ca83c3015828a44d71adcaf0d
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-08-23 19:34:32 -0700

    simplify wyvern import - move top priority import to wyvern/__init__.py

--- wyvern/__init__.py
+++ wyvern/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+from wyvern.components.pipeline_component import PipelineComponent
 from wyvern.wyvern_logging import setup_logging
 from wyvern.wyvern_tracing import setup_tracing
 
 setup_logging()
 setup_tracing()
+
+
+__all__ = [
+    "PipelineComponent",
+]
