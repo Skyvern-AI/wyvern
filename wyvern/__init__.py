7728a2e502af841b50ac7e6efac8e876d7ca8ba6
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-08-25 12:42:44 -0700

    a lot more for starter template

--- wyvern/__init__.py
+++ wyvern/__init__.py
@@ -1,19 +1,59 @@
 # -*- coding: utf-8 -*-
-from wyvern.components.models.model_component import ModelComponent
+from wyvern.components.features.realtime_features_component import (
+    RealtimeFeatureComponent,
+)
+from wyvern.components.models.model_component import (
+    ModelComponent,
+    ModelInput,
+    ModelOutput,
+)
 from wyvern.components.pipeline_component import PipelineComponent
-from wyvern.components.ranking_pipeline import RankingPipeline, RankingResponse
+from wyvern.components.ranking_pipeline import (
+    RankingPipeline,
+    RankingRequest,
+    RankingResponse,
+)
+from wyvern.entities.candidate_entities import CandidateSetEntity
+from wyvern.entities.feature_entities import FeatureData, FeatureMap
+from wyvern.entities.identifier import CompositeIdentifier, Identifier, IdentifierType
+from wyvern.entities.identifier_entities import (
+    ProductEntity,
+    QueryEntity,
+    UserEntity,
+    WyvernDataModel,
+    WyvernEntity,
+)
+from wyvern.feature_store.feature_server import generate_wyvern_store_app
 from wyvern.service import WyvernService
 from wyvern.wyvern_logging import setup_logging
 from wyvern.wyvern_tracing import setup_tracing
+from wyvern.wyvern_typing import WyvernFeature
 
 setup_logging()
 setup_tracing()
 
 
 __all__ = [
+    "generate_wyvern_store_app",
+    "CandidateSetEntity",
+    "CompositeIdentifier",
+    "FeatureData",
+    "FeatureMap",
+    "Identifier",
+    "IdentifierType",
     "ModelComponent",
+    "ModelInput",
+    "ModelOutput",
     "PipelineComponent",
+    "ProductEntity",
+    "QueryEntity",
     "RankingPipeline",
     "RankingResponse",
+    "RankingRequest",
+    "RealtimeFeatureComponent",
+    "UserEntity",
+    "WyvernDataModel",
+    "WyvernEntity",
+    "WyvernFeature",
     "WyvernService",
 ]
