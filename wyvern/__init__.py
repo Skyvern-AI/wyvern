bb1550322e1c3cbc4cd46048984acee3b8ff4124
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-08-25 12:39:18 -0700

    ModelComponent, RankingPipeline RankingResponse

--- wyvern/__init__.py
+++ wyvern/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+from wyvern.components.models.model_component import ModelComponent
 from wyvern.components.pipeline_component import PipelineComponent
+from wyvern.components.ranking_pipeline import RankingPipeline, RankingResponse
 from wyvern.service import WyvernService
 from wyvern.wyvern_logging import setup_logging
 from wyvern.wyvern_tracing import setup_tracing
@@ -9,6 +11,9 @@ setup_tracing()
 
 
 __all__ = [
+    "ModelComponent",
     "PipelineComponent",
+    "RankingPipeline",
+    "RankingResponse",
     "WyvernService",
 ]
