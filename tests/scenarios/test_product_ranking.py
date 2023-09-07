67b793a40831ef6394ae487b883a4fd5a6ef4d96
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-09-07 00:45:41 -0700

    fix test

--- tests/scenarios/test_product_ranking.py
+++ tests/scenarios/test_product_ranking.py
@@ -384,6 +384,7 @@ async def test_hydrate(mock_redis):
         json=json_input,
         headers={},
         entity_store={},
+        model_score_map={},
         events=[],
         feature_map=FeatureMap(feature_map={}),
     )
@@ -447,6 +448,7 @@ async def test_hydrate__duplicate_brand(mock_redis__duplicate_brand):
         entity_store={},
         events=[],
         feature_map=FeatureMap(feature_map={}),
+        model_score_map={},
     )
     request_context.set(test_wyvern_request)
 
