3d3820c3f1d2b790ca5ad9dc51c991d37c8f54a9
Shuchang Zheng <wintonzheng0325@gmail.com>
2023-09-12 20:46:24 -0700

    what and why (#55)
    * what and why
    
    * update sdk_ref

--- wyvern/config.py
+++ wyvern/config.py
@@ -11,42 +11,44 @@ class Settings(BaseSettings):
     in production for secrets you do not wish to save in code
 
     Attributes:
-        ENVIRONMENT: The environment the service is running in.
-        PROJECT_NAME: The name of the project
-        REDIS_HOST: The host of the redis instance
-        REDIS_PORT: The port of the redis instance
+        ENVIRONMENT: The environment the service is running in. Default to `development`.
+        PROJECT_NAME: The name of the project. Default to `default`.
+        REDIS_HOST: The host of the redis instance. Default to `localhost`.
+        REDIS_PORT: The port of the redis instance. Default to `6379`.
 
-        WYVERN_API_KEY: The API key for the Wyvern API
-        WYVERN_BASE_URL: The base url of the Wyvern API
-        WYVERN_ONLINE_FEATURES_PATH: The path to the online features endpoint
-        WYVERN_HISTORICAL_FEATURES_PATH: The path to the historical features endpoint
-        WYVERN_FEATURE_STORE_URL: The url of the Wyvern feature store
+        WYVERN_API_KEY: The API key for the Wyvern API. Default to `""`, empty string.
+        WYVERN_BASE_URL: The base url of the Wyvern API. Default to `https://api.wyvern.ai`
+        WYVERN_ONLINE_FEATURES_PATH:
+            The path to the online features endpoint. Default to `/feature/get-online-features`.
+        WYVERN_HISTORICAL_FEATURES_PATH:
+            The path to the historical features endpoint. Default to `/feature/get-historical-features`.
+        WYVERN_FEATURE_STORE_URL: The url of the Wyvern feature store. Default to `https://api.wyvern.ai`.
 
-        SNOWFLAKE_ACCOUNT: The account name of the Snowflake instance
-        SNOWFLAKE_USER: The username of the Snowflake instance
-        SNOWFLAKE_PASSWORD: The password of the Snowflake instance
-        SNOWFLAKE_ROLE: The role of the Snowflake instance
-        SNOWFLAKE_WAREHOUSE: The warehouse of the Snowflake instance
-        SNOWFLAKE_DATABASE: The database of the Snowflake instance
-        SNOWFLAKE_OFFLINE_STORE_SCHEMA: The schema of the Snowflake instance
+        SNOWFLAKE_ACCOUNT: The account name of the Snowflake instance. Default to `""`, empty string.
+        SNOWFLAKE_USER: The username of the Snowflake instance. Default to `""`, empty string.
+        SNOWFLAKE_PASSWORD: The password of the Snowflake instance. Default to `""`, empty string.
+        SNOWFLAKE_ROLE: The role of the Snowflake instance. Default to `""`, empty string.
+        SNOWFLAKE_WAREHOUSE: The warehouse of the Snowflake instance. Default to `""`, empty string.
+        SNOWFLAKE_DATABASE: The database of the Snowflake instance. Default to `""`, empty string.
+        SNOWFLAKE_OFFLINE_STORE_SCHEMA: The schema of the Snowflake instance. Default to `PUBLIC`.
 
-        AWS_ACCESS_KEY_ID: The access key id for the AWS instance
-        AWS_SECRET_ACCESS_KEY: The secret access key for the AWS instance
-        AWS_REGION_NAME: The region name for the AWS instance
+        AWS_ACCESS_KEY_ID: The access key id for the AWS instance. Default to `""`, empty string.
+        AWS_SECRET_ACCESS_KEY: The secret access key for the AWS instance. Default to `""`, empty string.
+        AWS_REGION_NAME: The region name for the AWS instance. Default to `us-east-1`.
 
-        FEATURE_STORE_TIMEOUT: The timeout for the feature store
-        SERVER_TIMEOUT: The timeout for the server
+        FEATURE_STORE_TIMEOUT: The timeout for the feature store. Default to `60` seconds.
+        SERVER_TIMEOUT: The timeout for the server. Default to `60` seconds.
 
-        REDIS_BATCH_SIZE: The batch size for the redis instance
-        WYVERN_INDEX_VERSION: The version of the Wyvern index
-        MODELBIT_BATCH_SIZE: The batch size for the modelbit
+        REDIS_BATCH_SIZE: The batch size for the redis instance. Default to `100`.
+        WYVERN_INDEX_VERSION: The version of the Wyvern index. Default to `1`.
+        MODELBIT_BATCH_SIZE: The batch size for the modelbit. Default to `30`.
 
-        EXPERIMENTATION_ENABLED: Whether experimentation is enabled
-        EXPERIMENTATION_PROVIDER: The experimentation provider
-        EPPO_API_KEY: The API key for EPPO (an experimentation provider)
+        EXPERIMENTATION_ENABLED: Whether experimentation is enabled. Default to `False`.
+        EXPERIMENTATION_PROVIDER: The experimentation provider. Default to `ExperimentationProvider.EPPO.value`.
+        EPPO_API_KEY: The API key for EPPO (an experimentation provider). Default to `""`, empty string.
 
-        FEATURE_STORE_ENABLED: Whether the feature store is enabled
-        EVENT_LOGGING_ENABLED: Whether event logging is enabled
+        FEATURE_STORE_ENABLED: Whether the feature store is enabled. Default to `True`.
+        EVENT_LOGGING_ENABLED: Whether event logging is enabled. Default to `True`.
     """
 
     ENVIRONMENT: str = "development"
